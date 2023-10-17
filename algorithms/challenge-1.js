97517848377a2f708224ad07d3abb1b99aaaafb0
nemehimself <onyekachi_val@yahoo.com>
2023-10-17 02:10:23 +0100

    6 test passed and replication done

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,2 +1,14 @@
-function countCharOccurrences(str, char) {}
+function countCharOccurrences(str, char) {
+
+        let counter = 0;
+        
+        for (let i = 0; i < str.length; i++) {
+        
+        if (str.charAt(i) == char) {
+    
+            counter++;
+        }
+        }    
+            return counter;
+    }
 module.exports = countCharOccurrences;
